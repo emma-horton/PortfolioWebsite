9aba92dccdcddce81cb4a8d484195bea6102d13c
emma773 <emmahorton03@gmail.com>
2024-10-13 13:55:53 +0100

    add partical.js to header

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,3 +162,13 @@ tiles.forEach(tile => {
         }, { once: true });
     });
 });
+
+
+const cursor = document.createElement('div');
+cursor.id = 'cursor';
+document.body.appendChild(cursor);
+
+document.addEventListener('mousemove', function(e) {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
